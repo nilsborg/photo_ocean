eb4a493f51e10ea2787b2d0630861e052c1151b1
Nils Borchers <nils.borchers@sumup.com>
2014-05-10 05:10:39 +0200

    extracted config vars into own file
    Signed-off-by: Nils Borchers <nils.borchers@sumup.com>

---
+++ app/scripts/config.template.js
@@ -0,0 +1,20 @@
+/**
+ * GLOBALS
+ */
+
+/*global */
+var page = 1,
+    apiKey = 'YOUR_API_KEY_MAN',
+    userId = 'YOURSELF', // philosophical question i know but.. what can i say
+    perPage = 10,
+    totalPages = 0,
+    // s   small square 75 x 75
+    // q   large square 150x150
+    // t   Thumbnail, 100 longest side
+    // m   small, 240 on longest side
+    // n   small, 320 on longest side
+    // -   small, 500 on longest side
+    // z   mediujm 640, 640 on longest side
+    // b   big, 1024 on longest side*
+    // o   original
+    photoSize = 'z';
